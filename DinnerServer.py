b591299b6982d3b2e6e75c009b5409cd2fa4c9ea
doylelew <69176020+doylelew@users.noreply.github.com>
2023-08-31 11:42:18 -0400

    image regular expression matching

--- DinnerServer.py
+++ DinnerServer.py
@@ -1,7 +1,18 @@
+import yaml
 from flask import Flask
+import yaml
+from cooklang_handler import recipe_handler
+
+with open('config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 @app.route("/")
 def home():
-    return "Hello World"
+
+    all_recipes = recipe_handler.retrieve_all(config['RECIPE_DIR'])
+
+    return all_recipes
--- cooklang_handler/recipe_handler.py
+++ cooklang_handler/recipe_handler.py
@@ -1,10 +1,31 @@
 import os
+import re
 import cooklang
+
+def find_images(directory, recipe_name):
+    pattern = re.compile(rf"{recipe_name}(.?\d+)?.(?:jpg|jpeg|png)")
+    image_list = [file for file in os.listdir(directory) if pattern.match(file)]
+    return image_list
 
 def retrieve_all(directory):
     recipe_list = []
     for item in os.listdir(directory):
         if item.endswith(".cook"):
-            recipe_list.append({'name': f'{item.removesuffix(".cook") }', 'images': [], 'category': directory})
+            recipe_name = item.removesuffix(".cook")
+            recipe_list.append(
+                {'name': f'{recipe_name}',
+                 'images': find_images(directory, recipe_name),
+                 'category': directory}
+            )
+
+
+        if os.path.isdir(os.path.join(directory, item)):
+            recipe_list.extend(retrieve_all(f"{directory}/{item}"))
 
     return recipe_list
+
+def scan_directory(directory, cache_location):
+    full_recipes = retrieve_all(directory)
+    with open(os.path.join(cache_location, 'full_recipe_list.json'), 'w') as recipe_cache:
+        recipe_cache.write(full_recipes)
+
